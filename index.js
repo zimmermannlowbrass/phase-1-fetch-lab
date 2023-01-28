ec508efe9634463a6ec54481726134be228c63a5
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-28 10:11:54 -0600

    completed labwork

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then(function(json) {renderBooks(json)})
 }
 
 function renderBooks(books) {
